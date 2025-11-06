5321627da4bfbcd8d3eb0f2f498dacb0d38ce709
santanu sen <santanusen1012@gmail.com>
2025-11-05 23:49:57 -0500

    Commit Quiz Game Code

--- question_model.py
+++ question_model.py
@@ -2,5 +2,3 @@ class Question:
     def __init__(self, text, answer):
         self.text = text
         self.answer = answer
-# question1 = Question(data.question_data["text"],data.question_data["answer"])
-# print(question1.question,question1.answer)
